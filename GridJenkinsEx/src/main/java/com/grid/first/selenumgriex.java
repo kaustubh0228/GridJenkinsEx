688c7f5f96103e35e3bb339492e3bf2ec5f416fe
Kaustubh Aras <kaustubh.aras@gmail.com>
2020-02-02 18:57:12 +0530

    changes

--- GridJenkinsEx/src/main/java/com/grid/first/selenumgriex.java
+++ GridJenkinsEx/src/main/java/com/grid/first/selenumgriex.java
@@ -13,7 +13,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class SelenumGridEx {
+public class selenumgriex {
 
 	private static Wait<WebDriver> wait;
 	private static DesiredCapabilities capabillities;
@@ -23,7 +23,7 @@ public class SelenumGridEx {
 	public static void setUp() throws Exception {
 		
 		capabillities = DesiredCapabilities.chrome();
-		driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabillities);
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabillities);
 		capabillities.setBrowserName("chrome");
 		wait = new WebDriverWait(driver, 6000);
 	}
